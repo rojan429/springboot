f0f2dc27dc11d351e42d6d13409e4b0d006bc3cc
rojan429 <78186110+rojan429@users.noreply.github.com>
2021-02-20 22:23:20 -0800

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/DAO/BookRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.DAO;
+import com.example.demo.Entity.Book;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface BookRepository extends MongoRepository<Book, Integer> {
+}
---
+++ demo/src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+import com.example.demo.Entity.Book;
+import com.example.demo.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public Collection<Book> getBooks(){
+        return bookService.getBooks();
+    }
+
+    @PostMapping()
+    public Book postBook(@RequestBody Book book){
+        return bookService.createBook(book);
+    }
+
+}
